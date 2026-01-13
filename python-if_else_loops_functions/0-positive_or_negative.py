3762b597f6f03317eb3daf0602237f02a5808586
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-01-13 10:02:01 +0000

    fix indentation for consistency in output messages

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,12 +3,9 @@ import random
 
 number = random.randint(-10, 10)
 
-if number < 0: 
-  print(f"{number} is negative")
-
-elif number == 0:   
-  print(f"{number} is zero")
-
+if number < 0:
+    print(f"{number} is negative")
+elif number == 0:
+    print(f"{number} is zero")
 else:
-  print(f"{number} is positive" )
- 
+    print(f"{number} is positive")
