9e13725365c6452e56b45bfd6811be67c0ad085e
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-01-14 08:11:21 +0000

    Refactor uppercase function to simplify character conversion logic

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord('a') <= ord(c) <= ord('z'):
-            print("{}".format(chr(ord(c) - 32)), end="")
-        else:
-            print("{}".format(c), end="")
+        print(
+            "{}".format(
+                chr(ord(c) - 32) if ord('a') <= ord(c) <= ord('z') else c
+            ),
+            end=""
+        )
     print()
