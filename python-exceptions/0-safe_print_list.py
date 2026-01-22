7a167fb421db103fff447fecb38e377602b3331d
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-01-22 09:20:00 +0000

    Fix safe_print_list to add space between printed elements

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in range(x):
+    i = 0
+    while i < x:
         try:
-            print(my_list[i], end="")
+            print("{:d}".format(my_list[i]), end="")
             count += 1
+        except (ValueError, TypeError):
+            pass
         except IndexError:
             break
+        i += 1
     print()
     return count
