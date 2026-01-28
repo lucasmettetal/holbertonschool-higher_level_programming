1b895602161c4e7aec2fa94d73a5520484e8b742
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-01-28 12:38:34 +0000

    Remove width property setter and height property getter from Rectangle class

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -15,20 +15,6 @@ class Rectangle:
         """Get width."""
         return self.__width
 
-    @width.setter
-    def width(self, value):
-        """Set width."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Get height."""
-        return self.__height
-
     @height.setter
     def height(self, value):
         """Set height."""
@@ -52,7 +38,7 @@ class Rectangle:
         """Return string representation."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        
+
         rectangle_str = []
         for _ in range(self.__height):
             rectangle_str.append("#" * self.__width)
