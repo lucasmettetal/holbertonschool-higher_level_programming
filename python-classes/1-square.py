f61465f01622e71f6eff760e9e3c72c4ae0efe04
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-01-28 08:39:33 +0000

    Add module docstring to Square class for clarity

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class with a private instance attribute size.
+"""
+
+
 class Square:
     def __init__(self, size):
         self.__size = size
