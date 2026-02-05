569034ca39e0bd5df1a83d5e4fa2c9eabd7e6188
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-02-05 08:48:11 +0000

    Refactor docstring in MyList class for clarity and consistency

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""
-This module defines a class MyList that inherits from list.
-"""
+"""Defines a MyList class that inherits from list."""
 
 
-def print_sorted(self):
-    """Print the list, but sorted (ascending sort)."""
-    print(sorted(self))
+class MyList(list):
+    """Custom list class with a sorted print method."""
+
+    def print_sorted(self):
+        """Prints the list sorted in ascending order."""
+        print(sorted(self))
