5989bb2aabfbc63b61abca38282829f3c9f3e2cc
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-02-06 13:24:17 +0000

    Add initial implementation of Animal, Dog, and Cat classes

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,18 @@
+# task_00_abc.py
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self) -> str:
+        pass
+
+
+class Dog(Animal):
+    def sound(self) -> str:
+        return "Bark"
+
+
+class Cat(Animal):
+    def sound(self) -> str:
+        return "Meow"
