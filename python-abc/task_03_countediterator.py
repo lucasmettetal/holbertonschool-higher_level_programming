fd8bf78f11ec943d5823e21829df8e730c72123d
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-02-06 13:26:52 +0000

    Add CountedIterator class to track iteration count

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,17 @@
+# task_03_countediterator.py
+
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        item = next(self.iterator)   # may raise StopIteration
+        self.count += 1
+        return item
+
+    def get_count(self):
+        return self.count
