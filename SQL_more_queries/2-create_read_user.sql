0e83c586bde4d7525677ada3f21b3ad0976a09a3
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-03-04 10:12:21 +0000

    fix: remove unnecessary blank lines in SQL script for user creation

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,7 @@
 --create a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
 -- create a user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
-
 -- give read privileges on the database
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
