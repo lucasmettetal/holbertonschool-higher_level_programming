d2d74d593c1ffa51d5068e5b37f2ce98448fb0a3
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-03-04 10:19:34 +0000

    feat: update comments to specify database and user creation details

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
---create a database, a user and give that user read privileges on the database
+-- create a new database hbtn_0d_2 and a new user user_0d_2 with password user_0d_2_pwd
+-- give user_0d_2 only SELECT privileges on the hbtn_0d_2 database
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
