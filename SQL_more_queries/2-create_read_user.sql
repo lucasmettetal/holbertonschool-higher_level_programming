0cfaa734989f46e821829c34a956f467b5a41e45
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-03-04 10:13:13 +0000

    feat: update comment to clarify database and user creation with read privileges

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,5 @@
---create a database
+--create a database, a user and give that user read privileges on the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- create a user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
--- give read privileges on the database
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
