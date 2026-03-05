0243fabba831e1713e8bfffb6b5947736a7cdc7a
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-03-05 08:58:55 +0000

    feat: add SQL query to list titles of TV shows in the "Comedy" genre ordered by title

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- List the titles of all TV shows that belong to the "Comedy" genre. Order the results by title in ascending order.
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
