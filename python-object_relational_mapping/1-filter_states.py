d4b307960fd03a96b7024dfa3cae8526a27e25b2
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-03-06 09:56:14 +0000

    refactor: improve variable naming and enhance docstring clarity in state filtering script

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""Print states starting with uppercase N."""
+"""Print all states starting with uppercase N."""
 
 import MySQLdb
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
-    connection = MySQLdb.connect(
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
-    cur = connection.cursor()
-    request = "SELECT * FROM states WHERE nameLIKE BINARY 'N%' ORDER BY id ASC"
-    cur.execute(request)
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    )
 
-    for item in cur.fetchall():
-        print(item)
+    for row in cur.fetchall():
+        print(row)
 
     cur.close()
-    connection.close()
+    db.close()
