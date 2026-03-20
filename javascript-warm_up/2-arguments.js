8911b44a10e010ad9a700799d0c68bc7f7376669
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-03-20 13:26:03 +0000

    refactor: streamline argument handling in 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const argv = process.argv;
-if (argv.length === 2) {
-    console.log('No argument');
-} else if (argv.length === 3) {
-    console.log('Argument found');
+const argCount = process.argv.length - 2;
+
+if (argCount === 0) {
+  console.log('No argument');
+} else if (argCount === 1) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
